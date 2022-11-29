fff946ebcd2a63b1ca886d57cbc584160f9a0a90
TAHiRLI <tahrli.tahirr@gmail.com>
2022-11-29 18:00:20 +0400

    firstCommit

---
+++ PustokApp/Pustok/DAL/PustokDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pustok.Models;
+
+namespace Pustok.DAL
+{
+    public class PustokDbContext:DbContext
+    {
+        public PustokDbContext(DbContextOptions<PustokDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Authors> Authors { get; set; }
+    }
+}
---
+++ PustokApp/Pustok/Migrations/20221129135742_AuthorsTableCreated.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pustok.Migrations
+{
+    public partial class AuthorsTableCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
